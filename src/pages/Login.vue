9fe13715fe36d10cbc06e172165768887215ef04
odyssey <klidaval.com@gmail.com>
2025-10-05 17:39:10 +0330

    Create Login.vue

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-semibold mb-4">Sign in</h2>
+    <form @submit.prevent="login" class="space-y-3">
+      <input v-model="username" placeholder="username" class="w-full p-2 bg-[#2a2a2a] rounded" />
+      <input v-model="password" placeholder="password" type="password" class="w-full p-2 bg-[#2a2a2a] rounded" />
+      <div class="flex justify-end">
+        <Button type="submit">Login</Button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Button from '../components/Button.vue'
+import { useRouter } from 'vue-router'
+const username = ref('admin')
+const password = ref('password')
+const router = useRouter()
+function login(){ /* demo-login */ router.push('/') }
+</script>
