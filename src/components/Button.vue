67bb3548e4c5b21e60306b864e31afbb855f6190
odyssey <klidaval.com@gmail.com>
2025-10-05 17:41:03 +0330

    Create Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,6 @@
+<template>
+  <button :class="['px-3 py-1 rounded bg-accent text-white hover:opacity-90', className]" @click="$emit('click')"><slot /></button>
+</template>
+<script setup>
+defineProps({ className: { type: String, default: '' } })
+</script>
