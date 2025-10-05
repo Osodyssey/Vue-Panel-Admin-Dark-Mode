f7f8e88dc24e6b82539f5d1f53e74d964b21b1fa
odyssey <klidaval.com@gmail.com>
2025-10-05 17:41:40 +0330

    Create Table.vue

---
+++ src/components/Table.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-bgSecondary p-3 rounded">
+    <div class="overflow-x-auto">
+      <table class="min-w-full text-left">
+        <thead><tr class="text-sm text-textLight/80"><th class="px-3 py-2">#</th><th class="px-3 py-2">Name</th><th class="px-3 py-2">Email</th><th class="px-3 py-2">Actions</th></tr></thead>
+        <tbody><slot /></tbody>
+      </table>
+    </div>
+  </div>
+</template>
