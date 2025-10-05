4dc802501e5354ecdb3c3d5e05707d7cb5abe28d
odyssey <klidaval.com@gmail.com>
2025-10-05 17:40:20 +0330

    Create AuthLayout.vue

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-bgPrimary text-textLight">
+    <div class="w-full max-w-md p-6 bg-bgSecondary rounded-lg shadow-lg">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
