7d3d95c8d898f47862b2e6bb7a8fc63b58b7134a
odyssey <klidaval.com@gmail.com>
2025-10-05 17:41:25 +0330

    Create Modal.vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="fixed inset-0 bg-black/50 flex items-center justify-center">
+    <div class="bg-bgSecondary p-4 rounded w-full max-w-lg">
+      <div class="flex justify-between items-center mb-3"><slot name="title" /><button @click="$emit('close')">✕</button></div>
+      <div><slot /></div>
+    </div>
+  </div>
+</template>
